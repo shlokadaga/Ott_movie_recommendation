2815d3bdbe20811db31b8ff48b012b1e0224c185
shlokadaga <60999620+shlokadaga@users.noreply.github.com>
2023-03-07 20:24:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import requests
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 import gspread as gs
 import streamlit as st
